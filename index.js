63bbc96a83d456011df9b1307a50682b5af8fc48
심규장 Gyu Jang Sim <gyujang95@gmail.com>
2020-12-28 23:06:39 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const PORT = 4000;
+
+const handleHome = (req, res) => res.send('hello world');
+
+const handleProfile = (req, res) => res.send('this is profile ');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
